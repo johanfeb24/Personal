d4a89eaa1ba5df82d5de69de45920b50f667b8a2
johanfeb24 <johantirado24@gmail.com>
2025-03-31 20:30:18 -0500

    Salario empleados

--- PlantillaAprendizaje/Program.cs
+++ PlantillaAprendizaje/Program.cs
@@ -10,20 +10,51 @@ namespace PlantillaAprendizaje
     {
         static void Main(string[] args)
         {
-            double sumAltura=0, promAltura;
-            Console.WriteLine("¿Cuantas alturas desea ingresar?");
-            int cantAlturas= int.Parse(Console.ReadLine());
+            double importe = 0, empleadoCatg1 = 0, empleadoCatg2 = 0;
+            Console.WriteLine("¿Cuántos empleados trabajan en la empresa?");
+            int cantEmpleados = int.Parse(Console.ReadLine());
 
-            for(int i=0; i<cantAlturas; i++)
+            for (int i = 0; i < cantEmpleados; i++)
             {
-                Console.WriteLine($"Ingrese la altura {i+1}: ");
-                double altura=double.Parse(Console.ReadLine());
-                sumAltura += altura;                
+                double salario;
+                while (true) // Bucle para validar el salario
+                {
+                    Console.WriteLine($"Ingrese el salario del empleado # {i + 1}: ");
+                    if (double.TryParse(Console.ReadLine(), out salario))
+                    {
+                        if (salario >= 100)
+                        {
+                            break; // Salimos del bucle si el salario es válido
+                        }
+                        else
+                        {
+                            Console.WriteLine("No tenemos un salario tan bajo en la empresa. Ingrese un valor válido.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada no válida. Ingrese un número.");
+                    }
+                }
+
+                importe += salario;
+
+                if (salario >= 100 && salario <= 300)
+                {
+                    empleadoCatg1++;
+                }
+                else
+                {
+                    empleadoCatg2++;
+                }
             }
-            promAltura = sumAltura / cantAlturas;
-            Console.WriteLine($"El promedio de altura de las personas censadas es: {promAltura}");
-            Console. ReadLine();
+
+            Console.WriteLine($"La cantidad de empleados que ganan entre $100 y $300 es: {empleadoCatg1}");
+            Console.WriteLine($"La cantidad de empleados que ganan más de $300 es: {empleadoCatg2}");
+            Console.WriteLine($"El importe total de la empresa es: {importe}");
+            Console.ReadLine();
         }
-            
     }
 }
+
+
