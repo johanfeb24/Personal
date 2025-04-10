f399b52cd1e81a25ccd32e0f7269b6f925a95a1d
johanfeb24 <johantirado24@gmail.com>
2025-04-09 19:26:04 -0500

    Serie del 11

--- PlantillaAprendizaje/Program.cs
+++ PlantillaAprendizaje/Program.cs
@@ -10,14 +10,13 @@ namespace PlantillaAprendizaje
     {
         static void Main(string[] args)
         {
-            Console.Write("Ingrese un número para ver su tabla de multiplicar: ");
-            int numero = int.Parse(Console.ReadLine());
+            int numero = 11;
 
-            Console.WriteLine($"Tabla del {numero}:");
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 25; i++)
             {
-                Console.WriteLine($"{numero} x {i} = {numero * i}");
-            }
+                Console.WriteLine(numero);
+                numero +=11;
+            }            
         }
     }
 }
